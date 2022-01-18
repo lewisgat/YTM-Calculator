b043b0d3576e83d445ee1ae3714721412c551e7c
lewisgat <49658302+lewisgat@users.noreply.github.com>
2022-01-18 18:15:16 +0000

    Update YTM_Calculator.cs

--- YTM_Calculator.cs
+++ YTM_Calculator.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 
 //Author: Lewis Broderick-Gatrell
@@ -50,13 +50,15 @@ namespace YTM_Calculator
 
         public double Bond_Price(DateTime PricingDate, double r)  //calculates a bonds price at a given yield and date using the cashflows within this class. Returns the value as a percentage of par
         {
+            int period = 1;
             double price = 0;
             double par = Total_Principle();
             foreach (Cashflow c in Flows)
             {
                 DateTime enddate = c.paymentDate;
-                double years = (enddate.Date - PricingDate.Date).Days / 365;   //assuming 365 days a year each year in order to get years until this coupon/principle payment
-                price += (c.PrincipleAmount + c.CouponAmount) / Math.Pow(1 + r, years);
+                //double years = (enddate.Date - PricingDate.Date).Days / 365;   //assuming 365 days a year each year in order to get years until this coupon/principle payment
+                price += (c.PrincipleAmount + c.CouponAmount) / Math.Pow(1 + r, period);
+                period++;
             }
             return price/par * 100;
         }
@@ -66,12 +68,13 @@ namespace YTM_Calculator
                                                                                         this function is used in the Newton-Raphson algorithm in order to find the root which is the YTM */
         {
             double total_principle = Total_Principle();
-            double p = PresentValuePar * total_principle;           
-
+            double p = PresentValuePar * total_principle;
+            int period = 1; 
             foreach (Cashflow c in Flows)
             { DateTime enddate = c.paymentDate;
-                double years = (enddate.Date - PricingDate.Date).Days/365.0;   //assuming 365 days a year each year in order to get the number of years until this coupon/principle payment
-                p -= (c.PrincipleAmount + c.CouponAmount) / Math.Pow(1+r, years);                                                            
+                // double years = (enddate.Date - PricingDate.Date).Days/365.0;   //assuming 365 days a year each year in order to get the number of years until this coupon/principle payment
+                p -= (c.PrincipleAmount + c.CouponAmount) / Math.Pow(1+r, period);
+                period++ ;
 
             }
 
@@ -80,14 +83,16 @@ namespace YTM_Calculator
 
         private double Function_Derivative(double r, DateTime PricingDate)  //derivative of the function defined above. Needed for the Newton-Raphson algorithm
         {
+            int period = 1; 
             double s = 0;           /*set initial price of bond to be 0 the calculator then iterates through 
                                      each cashflow */
             foreach (Cashflow c in Flows)
             {
                 DateTime enddate = c.paymentDate;
-                double years = (enddate.Date - PricingDate.Date).Days / 365.0;   //assuming 365 days a year in order to get years until this coupon/principle payment
-                s += years*(c.PrincipleAmount + c.CouponAmount) / Math.Pow(1 + r, 1+years);
 
+                //double years = (enddate.Date - PricingDate.Date).Days / 365.0;   //assuming 365 days a year in order to get years until this coupon/principle payment
+                s += period * (c.PrincipleAmount + c.CouponAmount) / Math.Pow(1 + r, period - 1);
+                period++;
             }
 
             return s;
@@ -128,29 +133,29 @@ namespace YTM_Calculator
             cashflows.Add_Flow(new DateTime(2027, 11, 20), 0, 22750);
             cashflows.Add_Flow(new DateTime(2028, 05, 20), 1000000, 22750);
 
-            //YTM for a bond priced at 102% of par as of 27/03/2021
+            //Question A: YTM for a bond priced at 102% of par as of 27/03/2021
             double ParValue1 = 1.02;
             double[] rA = cashflows.YTM_Calculator(new DateTime(2021, 03, 27), ParValue1);
-            Console.WriteLine("The YTM was calculated to be " + rA[0]*100 + "%.\nThis took the Newton-Raphson algorithm " + rA[1] +" iterations to compute.");
+            Console.WriteLine("The YTM was  calculated to be " + Math.Round(rA[0] * 100, 4) + "%.\nThis took the Newton-Raphson algorithm " + rA[1] + " iterations to compute.");
             Console.WriteLine("\n");
 
-            //YTM for a bond priced at 98% of par as of 27/03/2021
+            //Question B: YTM for a bond priced at 98% of par as of 27/03/2021
             double ParValue2 = 0.98;
             double[] rB = cashflows.YTM_Calculator(new DateTime(2021, 03, 27), ParValue2);
-            Console.WriteLine("The YTM was calculated to be " + rB[0] * 100 + "%.\nThis took the Newton-Raphson algorithm " + rB[1] + " iterations to compute.");
+            Console.WriteLine("The YTM was calculated to be " + Math.Round(rB[0] * 100, 4) + "%.\nThis took the Newton-Raphson algorithm " + rB[1] + " iterations to compute.");
             Console.WriteLine("\n");
 
-            //YTM for a bond priced 102% of par as of 07/04/2021
+            //Question C: YTM for a bond priced 102% of par as of 07/04/2021
             double[] rC = cashflows.YTM_Calculator(new DateTime(2021, 04, 07), ParValue1);
-            Console.WriteLine("The YTM was calculated to be " + rC[0] * 100 + "%.\nThis took the Newton-Raphson algorithm " + rC[1] + " iterations to compute.");
+            Console.WriteLine("The YTM was calculated to be " + Math.Round(rC[0] * 100, 4) + "%.\nThis took the Newton-Raphson algorithm " + rC[1] + " iterations to compute.");
             Console.WriteLine("\n");
 
-            //Calculating the bond price at 4.22% yield as of 27/03/2021
+            //Question D: Calculating the bond price at 4.22% yield as of 27/03/2021
             double bp = cashflows.Bond_Price(new DateTime(2021, 03, 27), 0.0422);
-            Console.WriteLine("The Bond Price is " + bp + "% of par");
+            Console.WriteLine("The Bond Price is " + Math.Round(bp, 4) + "% of par");
             Console.WriteLine("\n");
 
-            //Additional Cashflows are added and the calculations above are repeated
+            //Question E: Additional Cashflows are added and the calculations above are repeated
             Cashflows cashflow2 = cashflows;
             cashflow2.Add_Flow(new DateTime(2021, 05, 20), 0, 22750);
             cashflow2.Add_Flow(new DateTime(2021, 11, 20), 0, 22750);
@@ -162,22 +167,22 @@ namespace YTM_Calculator
 
             //YTM for a bond priced 102% of par as of 27/03/2021
             double[] r1 = cashflow2.YTM_Calculator(new DateTime(2021, 03, 27), ParValue1);
-            Console.WriteLine("The YTM was calculated to be " + r1[0] * 100 + "%.\nThis took the Newton-Raphson algorithm " + r1[1] + " iterations to compute.");
+            Console.WriteLine("The YTM was calculated to be " + Math.Round(r1[0] * 100, 4) + "%.\nThis took the Newton-Raphson algorithm " + r1[1] + " iterations to compute.");
             Console.WriteLine("\n");
 
             //YTM for a bond priced at 98% of par as of 27/03/2021
             double[] r2 = cashflow2.YTM_Calculator(new DateTime(2021, 03, 27), ParValue2);
-            Console.WriteLine("The YTM was calculated to be " + r2[0] * 100 + "%.\nThis took the Newton-Raphson algorithm " + r2[1] + " iterations to compute.");
+            Console.WriteLine("The YTM was calculated to be " + Math.Round(r2[0] * 100, 4) + "%.\nThis took the Newton-Raphson algorithm " + r2[1] + " iterations to compute.");
             Console.WriteLine("\n");
 
             //YTM for a bond priced at 102% of par as of 07/04/2021
             double[] r3 = cashflow2.YTM_Calculator(new DateTime(2021, 04, 07), ParValue1);
-            Console.WriteLine("The YTM was calculated to be " + r3[0] * 100 + "%.\nThis took the Newton-Raphson algorithm " + r3[1] + " iterations to compute.");
+            Console.WriteLine("The YTM was calculated to be " + Math.Round(r3[0] * 100, 4) + "%.\nThis took the Newton-Raphson algorithm " + r3[1] + " iterations to compute.");
             Console.WriteLine("\n");
 
             //Calculating the bond price at 4.22% yield as of 27/03/2021
             double bpE = cashflow2.Bond_Price(new DateTime(2021, 03, 27), 0.0422);
-            Console.WriteLine("The Bond Price is " + bpE +"% of par");
+            Console.WriteLine("The Bond Price is " + Math.Round(bpE, 4) +"% of par");
 
         }
     }
